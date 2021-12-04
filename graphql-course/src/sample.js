14c8a9805a528997ca948abf4af792fef34d3e36
AicardoS <aiesuarezba@unal.edu.co>
2021-12-04 14:27:16 -0500

    proyecto backend, CRUD: MONGO-EXPRESS-GRAPHQL

---
+++ graphql-course/src/sample.js
@@ -0,0 +1,20 @@
+export const tasks =[
+    {
+        _id:0,
+        tittle: 'actividad 1',
+        description: 'levantarse',
+        number: 100
+    },
+    {
+        _id:1,
+        tittle: 'actividad 2',
+        description: 'correr',
+        number: 100
+    },
+    {
+        _id:2,
+        tittle: 'actividad 3',
+        description: 'no hago más naa',
+        number: 100
+    }
+]
