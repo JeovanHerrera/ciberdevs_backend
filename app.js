c03d030c11c29bea58dd61418da95a00b9ddab1d
jeovan <jeovanis@outlook.com>
2021-12-21 07:01:47 -0500

    agregar variable de entorno puerto

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ app.use(
 )
 // Este es el uri para la conexion a MongoDB se trae desde Mongo Atlas
 // Usamos process.env para obtener los valores desde Nodemon
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.pypi6.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-//const uri = `mongodb+srv://ciberdevsc4:ciberdevsc4@cluster0.pypi6.mongodb.net/ciberdevsc4?retryWrites=true&w=majority`
+//const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.pypi6.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const uri = `mongodb+srv://ciberdevsc4:ciberdevsc4@cluster0.pypi6.mongodb.net/ciberdevsc4?retryWrites=true&w=majority`
 // Declaramos las opciones para mongoose
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 
 // Usamos mongose para conectarnos al uri con las opciones y entonces escucharlas en el puerto 3000 usando app
 mongoose
   .connect(uri, options)
-  .then(() => app.listen(process.env.PORT || 4000, console.log("Server is running")))
+  .then(() => app.listen(process.env.PORT || 4000))
   .catch(error => {
     throw error
   })
